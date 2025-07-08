56edffac087e2031ebbaf15c39448f0de1a77ce8
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2025-07-08 10:03:04 +0530

    css module

--- css-part/app/(user)/blog/page.jsx
+++ css-part/app/(user)/blog/page.jsx
@@ -2,6 +2,7 @@ const Blog=()=>{
     return(
         <>
         <h1 className="flex justify-center align-middle pt-20 text-9xl text-sky-400">Blog</h1>
+         <h3 className="common">Global CSS</h3>
         </>
     )
 }
--- css-part/app/(user)/service/page.jsx
+++ css-part/app/(user)/service/page.jsx
@@ -1,7 +1,9 @@
-const Service=()=>{
-    return(
+import Style from "./service.module.css"
+const Service = () => {
+    return (
         <>
-        <h1 className="flex justify-center align-middle pt-20 text-9xl text-red-400">Service</h1>
+            <h1 className="flex justify-center align-middle pt-20 text-9xl text-red-400">Service</h1>
+            <h3 className={Style.common}>Module CSS</h3>
         </>
     )
 }
